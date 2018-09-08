f362cbbbbedb7bc0cc08a25b535b71ea862a6207
shsg <alessia.rupf@student.unisg.ch>
2018-09-08 15:17:03 +0200

    pulling from Master

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
-<<<<<<< HEAD
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
-});
-
-// Static files
-=======
 var server = app.listen(4000,function(){
-  console.log('listening to requests on port 4000');
+    console.log('listening for requests on port 4000');
 });
 
 //Static files
->>>>>>> 0df570d9c5cd24b0d4253bad8fc925bae091e6e4
 app.use(express.static('public'));
+
+//socket setup
+var io = socket(server);
+
+io.on('connection',function(socket){
+  console.log('made socket connection', socket.id)
+});
