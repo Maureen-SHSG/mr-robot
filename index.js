666fa2a5a1d5a7383b33bf4f1876a8b414d5363e
shsg <alessia.rupf@student.unisg.ch>
2018-09-10 09:45:02 +0200

    hello woorld

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(express.static('public'));
 // Socket setup & pass server
 var io = socket(server);
 io.on('connection', function(socket){
-  
+
     console.log('made socket connection', socket.id);
 
     socket.on('chat',function(data){
       io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing',function(data){
+      socket.broadcast.emit('typing',data)
     });
 });
